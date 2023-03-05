52fa2bae3194603fc0faac6f2daddb9d2e3f7021
ruslanuskembaev <105996765+ruslanuskembaev@users.noreply.github.com>
2023-03-05 20:55:06 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,135 @@
+const { query } = require("express");
+
+console.log("hello");
+
+let a = document.getElementsByClassName('row');
+a.innerHTML= ('<p>loool</p>');
+
+//function table(){
+
+
+  //  }
+
+
+    // У каждого пользователя будет случайный стиль для блока с сообщенями,
+		// поэтому в этом кусочке кода мы получаем случайные числа
+		var min = 1;
+		var max = 6;
+		var random = Math.floor(Math.random() * (max - min)) + min;
+
+		// Устаналиваем класс в переменную в зависимости от случайного числа
+		// Эти классы взяты из Bootstrap стилей
+		var alertClass;
+		switch (connections.length) {
+			case 1:
+				alertClass = 'secondary';
+				break;
+			case 2:
+				alertClass = 'danger';
+				break;
+			case 3:
+				alertClass = 'success';
+				break;
+			case 4:
+				alertClass = 'warning';
+				break;
+			case 5:
+				alertClass = 'info';
+				break;
+			case 6:
+				alertClass = 'light';
+				break;
+		}
+
+function table(){
+
+
+	let a = document.getElementsByClassName('row');
+
+	let b = document.getElementById("message1");
+
+	let tbl = document.querySelector('table')
+
+	document.createElement('table');
+
+	let table = document.createElement('table');
+	let thead = document.createElement('thead');
+	let tbody = document.createElement('tbody');
+	
+	table.appendChild(thead);
+	table.appendChild(tbody);
+	
+	let row_1 = document.createElement("tr");
+	
+	let m1 = document.getElementById('message1');
+
+	let heading_1 = document.createElement("th");
+	heading_1.innerHTML = ("Игрок:");
+	let heading_2 = document.createElement("th");
+	heading_2.innerHTML = "Имя"+" " +m1.value;
+	let heading_3 = document.createElement("th");
+	heading_3.innerHTML = "Город";
+	let heading_4 = document.createElement("th");
+	heading_4.innerHTML = "Растение";
+	let heading_5 = document.createElement("th");
+	heading_5.innerHTML = "Животное";
+	let heading_6 = document.createElement("th");
+	heading_6.innerHTML = "Река";
+	
+	row_1.appendChild(heading_1);
+	row_1.appendChild(heading_2);
+	row_1.appendChild(heading_3);
+	row_1.appendChild(heading_4);
+	row_1.appendChild(heading_5);
+	row_1.appendChild(heading_6);
+	
+	tbl.appendChild(row_1);;
+	
+	a.innerHTML= (tbl);
+
+	
+
+}
+
+
+
+
+		// Функция для работы с данными на сайте
+		$(function() {
+
+			// Включаем socket.io и отслеживаем все подключения
+			var socket = io.connect();
+			// Делаем переменные на:
+			var $form = $("#messForm"); // Форму сообщений
+			var $name = $("#name"); // Поле с именем
+			var $textarea1 = $("#message1"); // Текстовое поле
+			var $textarea2 = $("#message2"); // Текстовое поле
+			var $textarea3 = $("#message3"); // Текстовое поле
+			var $textarea4 = $("#message4"); // Текстовое поле
+			var $textarea5 = $("#message5"); // Текстовое поле
+			var $textarea6 = $(".table"); // Текстовое поле
+			var $all_messages = $("#all_mess"); // Блок с сообщениями
+			
+
+			// Отслеживаем нажатие на кнопку в форме сообщений
+			$form.submit(function(event) {
+				// Предотвращаем классическое поведение формы
+				event.preventDefault();
+				// В сокет отсылаем новое событие 'send mess',
+				// в событие передаем различные параметры и данные
+				socket.emit('send mess', {mess:  $textarea1.val() + $textarea2.val() + $textarea3.val() + $textarea4.val() + $textarea5.val(), name: $name.val(), className: alertClass});
+				// Очищаем поле с сообщением
+				$textarea.val('');
+			
+			});
+
+			// Здесь отслеживаем событие 'add mess', 
+			// которое должно приходить из сокета в случае добавления но ого сообщения
+			socket.on('add mess', function(data) {
+				// Встраиваем полученное сообщение в блок с сообщениями
+				// У блока с сообщением будет тот класс, который соответвует пользователю что его от правил
+				$all_messages.append("<div class='alert alert-" + data.className + "'><b>" + data.name + "</b>: " + data.mess + "</div>" + tbl);
+			
+			});
+
+		});

5d66ad306d13332d1c3b3ddf4153ffd37c8c2660
ruslanuskembaev <105996765+ruslanuskembaev@users.noreply.github.com>
2023-03-05 21:48:10 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -41,12 +41,95 @@ a.innerHTML= ('<p>loool</p>');
 				break;
 		}
 
+		function table2(){
+
+			let a = document.getElementsByClassName('row');
+
+			let m1 = document.getElementById("message1");
+			let m2 = document.getElementById("message2");
+			let m3 = document.getElementById("message3");
+			let m4 = document.getElementById("message4");
+			let m5 = document.getElementById("message5");
+		
+		
+			let tbl = document.querySelector('table')
+		
+			document.createElement('table');
+		
+			let table = document.createElement('table');
+			let thead = document.createElement('thead');
+			let tbody = document.createElement('tbody');
+			
+			table.appendChild(thead);
+			table.appendChild(tbody);
+			
+			let row_1 = document.createElement("tr");
+		
+		
+			let heading_1 = document.createElement("th");
+			heading_1.innerHTML = ("Игрок:");
+			let heading_2 = document.createElement("th");
+			heading_2.innerHTML = "Имя";
+			let heading_3 = document.createElement("th");
+			heading_3.innerHTML = "Город";
+			let heading_4 = document.createElement("th");
+			heading_4.innerHTML = "Растение";
+			let heading_5 = document.createElement("th");
+			heading_5.innerHTML = "Животное";
+			let heading_6 = document.createElement("th");
+			heading_6.innerHTML = "Река";
+			
+			row_1.appendChild(heading_1);
+			row_1.appendChild(heading_2);
+			row_1.appendChild(heading_3);
+			row_1.appendChild(heading_4); 
+			row_1.appendChild(heading_5);
+			row_1.appendChild(heading_6);
+			
+		
+			
+		  // Creating and adding data to second row of the table
+		  let row_2 = document.createElement("tr");
+		
+		  let row_2_data_1 = document.createElement("td");
+		  row_2_data_1.innerHTML = m1.value;
+		
+		  let row_2_data_2 = document.createElement("td");
+		  row_2_data_2.innerHTML = m2.value;
+		
+		  let row_2_data_3 = document.createElement("td");
+		  row_2_data_3.innerHTML = m3.value;
+		
+		  let row_2_data_4 = document.createElement("td");
+		  row_2_data_4.innerHTML = m4.value;
+		
+		  let row_2_data_5 = document.createElement("td");
+		  row_2_data_5.innerHTML = m5.value;
+		
+		
+		  row_2.appendChild(row_2_data_1);
+		  row_2.appendChild(row_2_data_2);
+		  row_2.appendChild(row_2_data_3);
+		  row_2.appendChild(row_2_data_4);
+		  row_2.appendChild(row_2_data_5);
+		
+		
+		  tbl.innerHTML="";
+		
+		
+		}
+
 function table(){
 
 
 	let a = document.getElementsByClassName('row');
 
-	let b = document.getElementById("message1");
+	let m1 = document.getElementById("message1");
+	let m2 = document.getElementById("message2");
+	let m3 = document.getElementById("message3");
+	let m4 = document.getElementById("message4");
+	let m5 = document.getElementById("message5");
+
 
 	let tbl = document.querySelector('table')
 
@@ -60,13 +143,12 @@ function table(){
 	table.appendChild(tbody);
 	
 	let row_1 = document.createElement("tr");
-	
-	let m1 = document.getElementById('message1');
+
 
 	let heading_1 = document.createElement("th");
 	heading_1.innerHTML = ("Игрок:");
 	let heading_2 = document.createElement("th");
-	heading_2.innerHTML = "Имя"+" " +m1.value;
+	heading_2.innerHTML = "Имя";
 	let heading_3 = document.createElement("th");
 	heading_3.innerHTML = "Город";
 	let heading_4 = document.createElement("th");
@@ -79,13 +161,43 @@ function table(){
 	row_1.appendChild(heading_1);
 	row_1.appendChild(heading_2);
 	row_1.appendChild(heading_3);
-	row_1.appendChild(heading_4);
+	row_1.appendChild(heading_4); 
 	row_1.appendChild(heading_5);
 	row_1.appendChild(heading_6);
 	
-	tbl.appendChild(row_1);;
+	tbl.appendChild(row_1);
 	
-	a.innerHTML= (tbl);
+  // Creating and adding data to second row of the table
+  let row_2 = document.createElement("tr");
+
+  let row_2_data_1 = document.createElement("td");
+  row_2_data_1.innerHTML = m1.value;
+
+  let row_2_data_2 = document.createElement("td");
+  row_2_data_2.innerHTML = m2.value;
+
+  let row_2_data_3 = document.createElement("td");
+  row_2_data_3.innerHTML = m3.value;
+
+  let row_2_data_4 = document.createElement("td");
+  row_2_data_4.innerHTML = m4.value;
+
+  let row_2_data_5 = document.createElement("td");
+  row_2_data_5.innerHTML = m5.value;
+
+
+  row_2.appendChild(row_2_data_1);
+  row_2.appendChild(row_2_data_2);
+  row_2.appendChild(row_2_data_3);
+  row_2.appendChild(row_2_data_4);
+  row_2.appendChild(row_2_data_5);
+
+
+  tbl.appendChild(row_2);
+
+
+
+	//a.innerHTML= (tbl);
 
 	
 
